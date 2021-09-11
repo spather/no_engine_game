49f5034bbba2e22826a33281f81aec14fdd288e8
Shyam Pather <shyam.pather@gmail.com>
2021-09-11 13:17:51 -0700

    <memory> should be included in shader_progam.h, not .cpp

--- src/lib/include/shader_program.h
+++ src/lib/include/shader_program.h
@@ -1,6 +1,7 @@
 #ifndef SHADER_PROGRAM_H
 #define SHADER_PROGRAM_H
 
+#include <memory>
 #include <string>
 
 #include <glad/glad.h>
--- src/lib/shader_program.cpp
+++ src/lib/shader_program.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <iterator>
 #include <fstream>
-#include <memory>
 #include <sstream>
 #include <string>
 
